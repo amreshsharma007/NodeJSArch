29a928b52be0553832305d9dcd50e5a503af2034
Amresh Kumar Sharma <amreshsharma007@gmail.com>
2024-08-02 14:00:28 +0530

    Minor changes

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -16,6 +16,14 @@ function dependencyLoader({
   agenda: Agenda;
 } {
   try {
+    /**
+     * WTF is going on here?
+     *
+     * We are injecting the mongoose models into the DI container.
+     * I know this is controversial but will provide a lot of flexibility at the time
+     * of writing unit tests, just go and check how beautiful they are!
+     */
+
     for (const m of models) {
       Container.set(m.name, m.model);
     }
--- src/loaders/models.ts
+++ src/loaders/models.ts
@@ -4,14 +4,6 @@ function setUpModels(): {
   name: string;
   model: mongoose.Model<never>;
 }[] {
-  /**
-   * WTF is going on here?
-   *
-   * We are injecting the mongoose models into the DI container.
-   * I know this is controversial but will provide a lot of flexibility at the time
-   * of writing unit tests, just go and check how beautiful they are!
-   */
-
   const userModel = {
     name: 'userModel',
     // Notice the required syntax and the '.default'
